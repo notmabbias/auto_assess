08dcb644befeea75816b8e21f3fb62ada032f3c3
notmabbias <sheehan1821@saskpolytech.ca>
2026-05-06 17:05:24 -0600

    worked on frontend

--- app.py
+++ app.py
@@ -5,10 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello World!</p>"
+    return render_template('base.html')
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template("hello.html",person=name)
-
